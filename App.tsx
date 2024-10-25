796d3fb20c367c7113f4482c17039d85701c2655
Mohancherukuri <91059311+Mohancherukuri@users.noreply.github.com>
2024-10-25 18:20:11 +0530

    Test Commit

--- App.tsx
+++ App.tsx
@@ -7,13 +7,7 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-} from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
